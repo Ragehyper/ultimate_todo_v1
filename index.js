98b2427cdd29f4a928e94c232e732cb090db4f51
Chuks1993 <chuksgrinage.dev@gmail.com>
2022-08-10 21:39:52 -0500

    Complete 🎉

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('lets goooo!!! 🚀🚀🚀');
+const todoList = document.getElementById('todoList');
+
+//* Create a new list item when clicking on the "Add" button
+function addTodoItem() {
+  const todoItem = document.createElement('li');
+  const todoInput = document.getElementById('todoInput');
+
+  if (todoInput.value === '') {
+    return alert('You must write something!');
+  }
+
+  todoItem.textContent = todoInput.value;
+  const closeBtn = document.createElement('SPAN');
+  closeBtn.style.marginLeft = '1em';
+  closeBtn.textContent = '\u00D7';
+  closeBtn.onclick = function () {
+    this.parentElement.remove();
+  };
+  todoItem.appendChild(closeBtn);
+  todoList.appendChild(todoItem);
+  todoInput.value = '';
+}
